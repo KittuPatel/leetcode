ee099ca5c7dcb36c4ef7af228d305f1fd3fbd461
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-07-11 18:43:22 -0700

    Time: 90 ms, Memory: 15.5 MB - LeetHub

--- third-maximum-number/third-maximum-number.py
+++ third-maximum-number/third-maximum-number.py
@@ -1,10 +1,44 @@
+from heapq import *
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        se = sorted(list(set(nums)))
-        n = len(se)
+#         se = sorted(list(set(nums)))
+#         n = len(se)
 
-        if n < 3:
-            return max(nums)
-        return se[n-3]
+        # if len(nums) < 3:
+        #     return max(nums)
+    
+    
+#         O(n) solution
+#         MAX1, MAX2, MAX3 = float('-inf'),float('-inf'),float('-inf')        
+#             for j in range(len(arr)):
+#                if arr[j] > MAX1:
+#                 MAX3 = MAX2
+#                 MAX2 = MAX1
+#                 MAX1 = arr[j]            
+#                elif arr[j] > MAX2 and arr[j] < MAX1:
+#                 MAX3 = MAX2
+#                 MAX2 = arr[j]
+#                elif arr[j] > MAX3 and arr[j] < MAX2:
+#                 MAX3 = arr[j]  
+
+#             if MAX3 == float('-inf'):
+#                return max(MAX2, MAX1)
+#             else:
+#                return MAX3
+
+
+#           O(n) Solution
+
+        S = []
+        
+        for x in set(nums):
+            if len(S) < 3:
+                heapq.heappush(S, x)
+            else:
+                heapq.heappushpop(S, x)
+                
+        return S[0] if len(S) == 3 else max(S)
+
+
 
             
