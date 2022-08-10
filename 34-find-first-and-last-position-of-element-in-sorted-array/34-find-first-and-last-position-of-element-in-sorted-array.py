6fe5f67887e93446dceb33a8eb1cb516c28fcefd
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-08-10 12:41:04 -0700

    Time: 133 ms (52.31%), Space: 15.4 MB (48.51%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,22 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        start = 0
-        end = len(nums) - 1
-        
-        res = [-1, -1]
-        
-        while start <= end:
-            mid = (start + end) // 2
-            
-            if nums[mid] == target:
-                res[0] = mid
-                end = mid - 1
-            elif nums[mid] > target:
-                end = mid - 1
-            else:
-                start = mid + 1
-        
-        start = 0
-        end = len(nums) - 1
-            
-        while start <= end:
-            mid = (start + end) // 2
-            
-            if nums[mid] == target:
-                res[1] = mid
-                start = mid + 1
-            elif nums[mid] > target:
-                end = mid - 1
-            else:
-                start = mid + 1
-        
-        return res
+        left = self.binSearch(nums, target, True)
+        right = self.binSearch(nums, target, False)
+        return [left, right]
+    
+    def binSearch(self, nums, target, leftBias):
+        l, r = 0, len(nums) - 1
+        i = -1
+        while l <= r:
+            m = (l+r) // 2
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else: # if nums[m] == target
+                i = m
+                if leftBias: # if I'm searching from left-side
+                    r = m - 1 
+                else: # if I'm searching from right-side
+                    l = m + 1
+        return i
