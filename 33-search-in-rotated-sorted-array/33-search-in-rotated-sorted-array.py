fadecf9e1fc46daf85eaaa7646bb1775dfd54673
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-08-09 19:03:34 -0700

    Time: 50 ms (81.08%), Space: 14.2 MB (91.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,56 +1,41 @@
 class Solution:
-    
-    def findPivot(self, nums):
-        start = 0
-        end = len(nums) - 1
-        while start <= end:
-            mid = start + ((end - start) // 2)
-            if mid < end and nums[mid] > nums[mid + 1]:
-                return mid
-            elif mid > start and nums[mid] < nums[mid - 1]:
-                return mid - 1
-            elif nums[mid] > nums[start]:
-                start = mid + 1
-            else:
-                end = mid - 1
-        return start
-        
-    def findMinimumElementIndex(self, nums):
-        start = 0
-        end = len(nums) - 1
-        n = len(nums)
-        while start <= end:
-            mid = start + ((end - start) // 2)
-
-            prev = (mid - 1 + n) % n
-            next = (mid + 1) % n
-
-            if nums[mid] <= nums[prev] and nums[mid] <= nums[next]:
-                return mid
-            elif nums[mid] <= nums[end]:
-                end = mid - 1
-            elif nums[mid] >= nums[start]:
-                start = mid + 1
-    
-        
-    def binarySearch(self, nums, start, end, target):
-        while start <= end:
-            mid = start + ((end - start) // 2)
-            if nums[mid] == target:
-                return mid
-            elif target < nums[mid]:
-                end = mid - 1
-            else:
-                start = mid + 1
-        return -1
-        
     def search(self, nums: List[int], target: int) -> int:
-        pivotIndex = self.findMinimumElementIndex(nums)
-        print(pivotIndex)
-        targetIndex = self.binarySearch(nums, 0, pivotIndex - 1, target)
-        return targetIndex if targetIndex != -1 else self.binarySearch(nums, pivotIndex, len(nums)-1, target)
         
-        # l = binary_search(self, nums, start, minIndex - 1, target)
-        # r = binary_search(self, nums, minIndex + 1, end, target)
-
+        def findMinimumElementIndex(self, nums):
+            start = 0
+            end = len(nums) - 1
+            n = len(nums)
+            while start <= end:
+                mid = start + ((end - start) // 2)
+                
+                prev = (mid - 1 + n) % n
+                next = (mid + 1) % n
+                
+                if nums[mid] <= nums[prev] and nums[mid] <= nums[next]:
+                    return mid
+                elif nums[mid] <= nums[end]:
+                    end = mid - 1
+                elif nums[mid] >= nums[start]:
+                    start = mid + 1
+    
+        
+        minIndex = findMinimumElementIndex(self, nums)
+        
+        def binary_search(self, nums, start, end):
+            while start <= end:
+                mid = start + ((end - start) // 2)
+                if nums[mid] == target:
+                    return mid
+                elif target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            return -1
+        
+        start = 0
+        end = len(nums) - 1
+        
+        l = binary_search(self, nums, start, minIndex - 1)
+        r = binary_search(self, nums, minIndex, end)
+        return max(l,r)
             
