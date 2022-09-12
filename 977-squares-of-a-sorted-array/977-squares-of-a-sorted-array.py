4cfad7b99804999c23afb9618649707a0ba59641
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-09-11 19:48:56 -0700

    Time: 300 ms (70.52%), Space: 16.1 MB (85.91%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -13,47 +13,47 @@ class Solution:
 
 #    O(n) solution but little but annoying
         
-#         for i in range(len(nums)):
-#             nums[i] = abs(nums[i])
+        # for i in range(len(nums)):
+        #     nums[i] = abs(nums[i])
     
-#         minVal = nums.index(min(nums))
-#         res = []
-#         left, right = minVal - 1, minVal + 1
+        minVal = nums.index(min(nums, key = abs))
+        res = []
+        left, right = minVal - 1, minVal + 1
         
-#         res.append(nums[minVal] * nums[minVal])
+        res.append(nums[minVal] * nums[minVal])
         
-#         while left >= 0 and right < len(nums):
-#             if nums[left] < nums[right]:
-#                 res.append(nums[left] * nums[left])
-#                 left -= 1
-#             else:
-#                 res.append(nums[right] * nums[right])
-#                 right += 1
+        while left >= 0 and right < len(nums):
+            if abs(nums[left]) < abs(nums[right]):
+                res.append(nums[left] * nums[left])
+                left -= 1
+            else:
+                res.append(nums[right] * nums[right])
+                right += 1
         
-#         while left >= 0:
-#             res.append(nums[left] * nums[left])
-#             left -= 1
+        while left >= 0:
+            res.append(nums[left] * nums[left])
+            left -= 1
         
-#         while right < len(nums):
-#             res.append(nums[right] * nums[right])
-#             right += 1
+        while right < len(nums):
+            res.append(nums[right] * nums[right])
+            right += 1
 
-#         return res
+        return res
 
 # O(n) using left and right pointers and finding max value and returning reverse
 
-        res = []
-        left, right = 0, len(nums) - 1
+#         res = []
+#         left, right = 0, len(nums) - 1
         
-        while left <= right:
-            if nums[left] * nums[left] > nums[right] * nums[right]:
-                res.append(pow(nums[left], 2))
-                left += 1
-            else:  
-                res.append(pow(nums[right], 2))
-                right -= 1
+#         while left <= right:
+#             if nums[left] * nums[left] > nums[right] * nums[right]:
+#                 res.append(pow(nums[left], 2))
+#                 left += 1
+#             else:  
+#                 res.append(pow(nums[right], 2))
+#                 right -= 1
         
-        return res[::-1]
+#         return res[::-1]
         
         
         
