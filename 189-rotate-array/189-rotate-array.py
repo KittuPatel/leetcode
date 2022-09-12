802fc5f76eaf5ad58c94df050246b0a4ce528788
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-09-11 20:55:53 -0700

    Time: 612 ms (24.76%), Space: 25.5 MB (6.98%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,9 +3,46 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            last = nums[len(nums) - 1]
-            nums.pop()
-            nums.insert(0, last)
+        #         Bad solution
+        #         for i in range(k):
+        #             last = nums[len(nums) - 1]
+        #             nums.pop()
+        #             nums.insert(0, last)
+
+        #         return nums
+
+        # O(n) space and time
+        #  put every element in i + k index
+        #         res = [0] * len(nums)
+        #         for idx, num in enumerate(nums):
+        #             pos = (idx + k) % len(nums)
+        #             print(pos)
+        #             res[pos] = num
+        #             print(num)
+
+        #         for i in range(len(nums)):
+        #             nums[i] = res[i]
+    
+        # O(n) Time | O(1) space solution   
+        # reverse nums, then again reverse first k elements and then reverse other              half
+        k = k % len(nums)
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
         
-        return nums
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+            
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        
+        
+        
+        
+
+        
