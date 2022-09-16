c080877363518ee293005297618a8475aaecd876
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-09-15 23:35:37 -0700

    Time: 60 ms (16.55%), Space: 13.9 MB (57.02%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,13 +1,23 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
+#     def climbStairs(self, n: int) -> int:
         
-        one, two = 1, 1
+#         one, two = 1, 1
         
-        for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
+#         for i in range(n-1):
+#             temp = one
+#             one = one + two
+#             two = temp
         
-        return one
+#         return one
+    
+    # Bottom up, O(n) space
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        res = [0 for i in range(n)]
+        res[0], res[1] = 1, 2
+        for i in range(2, n):
+            res[i] = res[i-1] + res[i-2]
+        return res[-1]
             
         
