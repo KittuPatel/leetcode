5781a54537f3cb3b779c2c4d360e20f46c49bfc8
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-09-20 15:50:11 -0700

    Time: 195 ms (14.26%), Space: 15.2 MB (13.59%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -2,7 +2,7 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
     # multiply from both front and back
-        A = nums
+        # A = nums
         B = nums[::-1]
         
         for i in range(1, len(nums)):
@@ -12,7 +12,7 @@ class Solution:
             if B[i-1] != 0:
                 B[i] *= B[i-1]
         
-        maxA = max(A)
+        maxA = max(nums)
         maxB = max(B)
         
         return max(maxA, maxB)
