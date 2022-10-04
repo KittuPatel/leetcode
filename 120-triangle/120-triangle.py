fd2d2ddc80251a42331c4ff1073e2e7cfa83b588
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-04 15:44:53 -0700

    Time: 76 ms (88.01%), Space: 14.8 MB (92.94%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -2,11 +2,10 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         # reference = leetcode video
         dp = [0] * (len(triangle) + 1)
-        
-        # for every row from reverse
+        res = 0
+        # dp = 0 0 0 0 0
         for row in triangle[::-1]:
             for i, num in enumerate(row):
                 dp[i] = num + min(dp[i], dp[i+1])
-                
         return dp[0]
-            
+    
