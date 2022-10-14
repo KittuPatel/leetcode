0c9ad195378e97a7a709ca1ac11fcd9e2293f4d7
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-14 01:15:03 -0700

    Time: 90 ms (74.41%), Space: 17.6 MB (92.29%) - LeetHub

--- 0243-shortest-word-distance/0243-shortest-word-distance.py
+++ 0243-shortest-word-distance/0243-shortest-word-distance.py
@@ -1,11 +1,27 @@
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+#         Bruteforce
+#         minDistance = len(wordsDict)
+        
+#         for i in range(len(wordsDict)):
+#             if wordsDict[i] == word1:
+#                 for j in range(len(wordsDict)):
+#                     if wordsDict[j] == word2:
+#                         minDistance = min(minDistance, abs(i-j))
+                        
+#         return minDistance
+
+
+        i1, i2 = -1, -1
         minDistance = len(wordsDict)
         
         for i in range(len(wordsDict)):
             if wordsDict[i] == word1:
-                for j in range(len(wordsDict)):
-                    if wordsDict[j] == word2:
-                        minDistance = min(minDistance, abs(i-j))
-                        
+                i1 = i
+            elif wordsDict[i] == word2:
+                i2 = i
+            
+            if i1 != -1 and i2 != -1:
+                minDistance = min(minDistance, abs(i1-i2))
+                
         return minDistance
