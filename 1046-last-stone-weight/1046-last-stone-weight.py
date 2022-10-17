455a25efecd9f446bd82e85fb6e0b9f217013cbb
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-16 17:06:01 -0700

    Time: 61 ms (36.49%), Space: 13.8 MB (62.02%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,20 +1,45 @@
+import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         
-        while len(stones) >= 1:
-            if len(stones) == 1:
-                return stones[0]
+        stones = [-s for s in stones]
+        
+        heapq.heapify(stones)
+        
+        # atleast 2 are there
+        while len(stones) > 1:
             
-            max1 = max(stones)
-            max1idx = stones.index(max1)
-            stones.pop(max1idx)
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
             
-            max2 = max(stones)
-            max2idx = stones.index(max2)
-            stones.pop(max2idx)
+            first = abs(first)
+            second = abs(second)
             
-            diff = abs(max1-max2)
-            if diff > 0: 
-                stones.append(diff)
+            diff = first - second
             
-        return 0
+            if first > second:
+                heapq.heappush(stones, diff*-1)
+            
+        stones.append(0)
+        return abs(stones[0])
+            
+            
+        
+#         while len(stones) >= 1:
+#             if len(stones) == 1:
+#                 return stones[0]
+            
+#             max1 = max(stones)
+#             max1idx = stones.index(max1)
+#             stones.pop(max1idx)
+            
+#             max2 = max(stones)
+#             max2idx = stones.index(max2)
+#             stones.pop(max2idx)
+            
+#             diff = abs(max1-max2)
+#             if diff > 0: 
+#                 stones.append(diff)
+            
+#         return 0
+
