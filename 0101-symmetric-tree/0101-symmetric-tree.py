969a79874612bd2bfffc0861cacd5c3b5daf609d
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-17 16:28:53 -0700

    Time: 34 ms (95.41%), Space: 14 MB (60.86%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,38 +6,58 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+    
         
-        def bfs(root):
-            res = []
-            q = collections.deque([root])
+        def dfs(root1, root2):
             
-            while q:
-                qLen = len(q)
-                level = []
-                
-                for _ in range(qLen):
-                    node = q.popleft()
-                    if not node: 
-                        level.append("N")
-                    else:
-                        level.append(node.val)
-                    if node:
-                        q.append(node.left)
-                        q.append(node.right)
-                        
-                if q:
-                    res.append(level)
-                    
-            return res
-        
-        
-        res = bfs(root)
-        print(res)
-        
-        for arr in res:
-            print(arr, arr[::-1])
-            if arr != arr[::-1]:
+            if not root1 and not root2:
+                return True
+            
+            if not root1 or not root2 or root1.val != root2.val:
                 return False
             
-        return True
+            
+            return dfs(root1.left, root2.right) and dfs(root1.right, root2.left)
+            
+        return dfs(root.left, root.right)
+        
+            
+            
+            
+        
+#         def bfs(root):
+#             res = []
+#             q = collections.deque([root])
+            
+#             while q:
+#                 qLen = len(q)
+#                 level = []
+                
+#                 for _ in range(qLen):
+#                     node = q.popleft()
+#                     if not node: 
+#                         level.append("N")
+#                     else:
+#                         level.append(node.val)
+#                     if node:
+#                         q.append(node.left)
+#                         q.append(node.right)
+                        
+#                 if q:
+#                     res.append(level)
+                    
+#             return res
+        
+        
+#         res = bfs(root)
+#         print(res)
+        
+#         for arr in res:
+#             print(arr, arr[::-1])
+#             if arr != arr[::-1]:
+#                 return False
+            
+#         return True
                         
