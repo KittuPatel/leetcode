fcce21d4cba3a3f238bb5cc28fdfad906713ebcf
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-17 21:15:08 -0700

    Time: 54 ms (60.24%), Space: 14.3 MB (22.03%) - LeetHub

---
+++ 0364-nested-list-weight-sum-ii/0364-nested-list-weight-sum-ii.py
@@ -0,0 +1,71 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        
+        temp = []
+        maxDepth = 0
+        q = collections.deque([(nestedList, 1)])
+        
+        while q:
+            cur, depth = q.popleft()
+            maxDepth = max(maxDepth, depth)
+            
+            for element in cur:
+                if element.isInteger():
+                    temp.append((element.getInteger(), depth))
+                else:
+                    if len(element.getList()) != 0:
+                        q.append((element.getList(), depth + 1))
+        
+        res = 0
+        for i in temp:
+            res += i[0] * (maxDepth - i[1] + 1)
+        
+        return res
+
+                
+                
+            
+        
