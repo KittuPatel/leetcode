11b133689b13b264e8123fdf7c3c7b395328db9b
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-18 00:08:56 -0700

    Time: 99 ms (48.86%), Space: 16.4 MB (71.44%) - LeetHub

---
+++ 0272-closest-binary-search-tree-value-ii/0272-closest-binary-search-tree-value-ii.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
+        res = []
+        def dfs(root):
+            if root:
+                dfs(root.left)
+                res.append(root.val)
+                dfs(root.right)
+            return res
+        
+        dfs(root)
+        
+        vals = sorted(res, key=lambda x: abs(target - x))
+        
+        return vals[:k]
