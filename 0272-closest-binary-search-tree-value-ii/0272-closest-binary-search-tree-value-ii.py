b158b2d522fc015d256e591181e03b55c9afab6f
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-18 00:24:36 -0700

    Time: 111 ms (30.95%), Space: 16.3 MB (90.75%) - LeetHub

--- 0272-closest-binary-search-tree-value-ii/0272-closest-binary-search-tree-value-ii.py
+++ 0272-closest-binary-search-tree-value-ii/0272-closest-binary-search-tree-value-ii.py
@@ -6,6 +6,7 @@
 #         self.right = right
 class Solution:
     def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
+        
         res = []
         def dfs(root):
             if root:
@@ -16,6 +17,43 @@ class Solution:
         
         dfs(root)
         
-        res.sort(key=lambda x: abs(target - x))
+        l = 0
+        r = k - 1
+        while r <= len(res) - 2:
+            if abs(res[r+1] - target) < abs(res[l] - target):
+                l += 1
+                r += 1
+            else:
+                return res[l: r+1]
+            
+        return res[l: r+1]
         
-        return res[:k]
+        
+        
+#         l
+        
+#         while right <= len(self.nodes) - 2:
+#             if abs(self.nodes[right + 1] - target) < abs(self.nodes[left] - target):
+#                 left += 1
+#                 right += 1
+#             else:
+#                 return self.nodes[left: right + 1]
+        
+#         return self.nodes[left: right + 1]
+        
+#  O (NLOGN) AND O(N)
+        #         res = []
+#         def dfs(root):
+#             if root:
+#                 dfs(root.left)
+#                 res.append(root.val)
+#                 dfs(root.right)
+#             return res
+        
+#         dfs(root)
+        
+#         res.sort(key=lambda x: abs(target - x))
+        
+#         return res[:k]
+
+
