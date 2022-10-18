f264e97647d36cc72b53297f20c6697db67fe7de
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-17 20:44:51 -0700

    Time: 122 ms (79.37%), Space: 21.9 MB (64.16%) - LeetHub

--- 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
+++ 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
@@ -1,21 +1,21 @@
-from collections import defaultdict
 class WordDistance:
 
     def __init__(self, wordsDict: List[str]):
-        self.hashmap = defaultdict(list)
-        
-        for idx, word in enumerate(wordsDict):
-            self.hashmap[word].append(idx)
-        
-        print(self.hashmap)
+        self.hashmap = {}
+        for i, word in enumerate(wordsDict):
+            if word not in self.hashmap:
+                self.hashmap[word] = [i]
+            else:
+                self.hashmap[word].append(i)
 
     def shortest(self, word1: str, word2: str) -> int:
         res = float("inf")
-        for w1i in self.hashmap[word1]:
-            for w2i in self.hashmap[word2]:
-                res = min(res, abs(w1i-w2i))  
-        
+        for word1idx in self.hashmap[word1]:
+            for word2idx in self.hashmap[word2]:
+                res = min(res, abs(word1idx - word2idx))
+                
         return res
+
 
 # Your WordDistance object will be instantiated and called as such:
 # obj = WordDistance(wordsDict)
