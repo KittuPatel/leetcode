e0fb67bb0fcfdd72f85e9160ffc7db3bf17b8026
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-18 20:13:59 -0700

    Time: 53 ms (81.89%), Space: 14 MB (49.90%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.py
+++ 0360-sort-transformed-array/0360-sort-transformed-array.py
@@ -1,11 +1,39 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
         
-        result = []
-        heapq.heapify(result)
+        res = []
         
-        for x in nums:
-            ans = (a*x*x) + (b*x) + c
-            heapq.heappush(result, ans)
+        for idx, num in enumerate(nums):
+            nums[idx] = (a*num*num) + (b*num) + c
             
-        return list(heapq.nsmallest(len(nums), result))
+        i, j = 0, len(nums)-1
+        # three cases 
+        if a > 0:
+            while i <= j:
+                if nums[i] > nums[j]:
+                    res.append(nums[i])
+                    i += 1
+                else:
+                    res.append(nums[j])
+                    j -= 1
+        
+        elif a < 0:
+            while i <= j:
+                if nums[i] < nums[j]:
+                    res.append(nums[i])
+                    i += 1
+                else:
+                    res.append(nums[j])
+                    j -= 1
+        
+        # if a == 0
+        else:
+            if b > 0:
+                return nums
+            else:
+                return nums[::-1]
+            
+        
+        return res if a < 0 else res[::-1]
+                
+            
