93229e84c68e80347fa89765c5a839595ab5b86c
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-18 23:49:16 -0700

    Time: 69 ms (98.72%), Space: 13.8 MB (93.62%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        
+        # calculate slope
+        hashmap = {}
+        max_points = 0
+        
+        for i in range(len(points)):
+            i_max = 0
+            for j in range(i+1, len(points)):
+                # slope = (y2 - y1)/(x2-x1)
+                # to avoid zero division error: denominator 
+                if (points[j][0] - points[i][0]) == 0:
+                    slope = float("inf")
+                else:
+                    slope = (points[j][1] - points[i][1]) / (points[j][0] - points[i][0])
+                
+                if slope in hashmap:
+                    hashmap[slope] += 1
+                else:
+                    hashmap[slope] = 1
+                    
+                i_max = i_max if i_max > hashmap[slope] else hashmap[slope]
+            hashmap.clear() 
+            max_points = max_points if max_points > i_max else i_max
+                    
+        return max_points+1
