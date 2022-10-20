17a55f176ce3e22e397cde088ac2b2dca77be4ea
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-19 17:27:47 -0700

    Time: 121 ms (84.78%), Space: 14.7 MB (89.70%) - LeetHub

--- 0254-factor-combinations/0254-factor-combinations.py
+++ 0254-factor-combinations/0254-factor-combinations.py
@@ -1,14 +1,17 @@
 class Solution:
     def getFactors(self, n: int) -> List[List[int]]:
-        def recur(n):
-            if n in memo: return memo[n]
-            ans = set()
-            for i in range(2, int(sqrt(n))+1):
-                if n%i == 0:
-                    ans.add((i, n//i))
-                    for j in recur(n//i):
-                        ans.add(tuple(sorted([i, *j])))
-            memo[n] = ans
-            return ans
-        memo = defaultdict(set)
-        return recur(n)
+        results = []
+        
+        def dfs(n, subset, start, result):
+
+            while start * start <= n:
+                if n % start == 0:
+                    result.append(subset + [start, n//start])
+                    dfs(n//start, subset + [start], start, result)
+
+                start += 1
+
+            return result
+        
+        dfs(n, [], 2, results)
+        return results
