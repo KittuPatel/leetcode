398f2572b92d85e88ed7461dc2fceb8f4cd45ebe
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-24 11:30:22 -0700

    Time: 111 ms (85.84%), Space: 21.7 MB (95.05%) - LeetHub

--- 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
+++ 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
@@ -1,19 +1,19 @@
 class WordDistance:
 
     def __init__(self, wordsDict: List[str]):
-        self.hashmap = {}
-        for i, word in enumerate(wordsDict):
-            if word not in self.hashmap:
-                self.hashmap[word] = [i]
-            else:
-                self.hashmap[word].append(i)
+        self.wordsMap = collections.defaultdict(list)
+        
+        for idx, word in enumerate(wordsDict):
+            self.wordsMap[word].append(idx)        
 
     def shortest(self, word1: str, word2: str) -> int:
         res = float("inf")
-        for word1idx in self.hashmap[word1]:
-            for word2idx in self.hashmap[word2]:
-                res = min(res, abs(word1idx - word2idx))
+        
+        for idx1 in self.wordsMap[word1]:
+            for idx2 in self.wordsMap[word2]:
+                res = min(res, abs(idx1-idx2))
                 
+        
         return res
 
 
