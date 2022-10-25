cb4a666432c8d35414ab37cbe1bbcb93bd35e173
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-10-24 20:53:50 -0700

    Time: 720 ms (97.41%), Space: 28 MB (40.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -2,7 +2,7 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxSub = nums[0]
         curSum = 0
-        
+        # Dynamic Programming, Kadane's Algorithm
         for end in range(len(nums)):
             if curSum < 0:
                 curSum = 0
