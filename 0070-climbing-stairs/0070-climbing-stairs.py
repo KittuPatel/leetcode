0b1b574c0ca452f6638f9ce1c00b5e5eae2360ef
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-11-05 19:56:31 -0700

    Time: 71 ms (5.17%), Space: 14 MB (12.10%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,24 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+#         one, two = 1, 1
         
-        one, two = 1, 1
+#         for i in range(n-1):
+#             temp = one
+#             one = one + two
+#             two = temp
         
-        for i in range(n):
-            temp = one
-            one = one + two
-            two = temp
-            
-        return two
+        
+#         return one
+
+        def climb(n):  # inner func to make life easier
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = climb(n-1) + climb(n-2)
+                return memo[n]
+        memo = {1: 1, 2: 2}  # base cases
+        return climb(n)
+
+     
+    
+        
