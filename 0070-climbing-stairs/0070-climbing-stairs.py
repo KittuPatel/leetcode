16e8f74c0e3e29cd713448248fd20fbdec12cfe2
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2022-11-05 19:59:26 -0700

    Time: 48 ms (69.43%), Space: 13.8 MB (96.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -10,15 +10,28 @@ class Solution:
         
 #         return one
 
-        def climb(n):  # inner func to make life easier
-            if n in memo:
-                return memo[n]
-            else:
-                memo[n] = climb(n-1) + climb(n-2)
-                return memo[n]
-        memo = {1: 1, 2: 2}  # base cases
-        return climb(n)
-
+        # def climb(n):  # inner func to make life easier
+        #     if n in memo:
+        #         return memo[n]
+        #     else:
+        #         memo[n] = climb(n-1) + climb(n-2)
+        #         return memo[n]
+        # memo = {1: 1, 2: 2}  # base cases
+        # return climb(n)
+        
+        if n == 1 or n == 2:
+            return n
+        
+        dp = [0] * n
+        
+        dp[0] = 1
+        dp[1] = 2
+        
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n-1]
+ 
      
     
         
