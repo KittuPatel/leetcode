0668cb8888dfee2b75261b7b59318bf0861a426c
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2023-03-27 20:23:06 -0700

    Time: 49 ms (76.32%), Space: 14.2 MB (44.78%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        ans = [-1] * len(nums1)
+        nums1IdxMap = {n:i for i, n in enumerate(nums1)}
+        
+        for i in range(len(nums2)):
+            if nums2[i] not in nums1IdxMap:
+                continue
+                
+            for j in range(i+1, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    idx = nums1IdxMap[nums2[i]]  
+                    ans[idx] = nums2[j]
+                    break
+                    
+        return ans
+            
+                
+                
+                
