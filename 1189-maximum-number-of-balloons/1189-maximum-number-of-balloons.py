a7e534e156033e893ed24e4753698121658dfb9f
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2023-04-02 19:00:09 -0700

    Time: 40 ms (27.97%), Space: 14.1 MB (5.54%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,28 +1,45 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         
-        d = {}
-        
+        countText = {}
         for char in text:
-            
-            d[char] = 1 + d.get(char, 0)
-            
-        print(d)
-        output = 0
+            countText[char] = 1 + countText.get(char, 0)
+                
+        balloon = {}
+        for char in "balloon":
+            balloon[char] = 1 + balloon.get(char, 0)
         
-        while True:
-            for char in "balloon":   
-                if char in d:
-                    d[char] -= 1
-                    if d[char] == 0:
-                        d.pop(char)
-                else:
-                    return output
+        
+        res = float("inf")
+        
+        for char in balloon:
+            if char in countText:
+                res = min(res, countText[char] // balloon[char])
+            else:
+                res = 0
+        
+        return res
+            
+            
+            
+            
+            
+#         print(d)
+#         output = 0
+        
+#         while True:
+#             for char in "balloon":   
+#                 if char in d:
+#                     d[char] -= 1
+#                     if d[char] == 0:
+#                         d.pop(char)
+#                 else:
+#                     return output
 
-            output += 1
+#             output += 1
                 
         
-        return output
+#         return output
             
             
             
