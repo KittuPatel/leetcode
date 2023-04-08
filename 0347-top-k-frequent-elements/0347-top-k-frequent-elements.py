2667ce6caea31a6890e08148308351af0caf2687
Krishna Sai Mangalarapu <herorocks56@gmail.com>
2023-04-07 19:58:47 -0700

    Time: 115 ms (27.23%), Space: 19.6 MB (11.91%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,18 +2,41 @@ import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        d = {}
+        hashmap = {}
+        
+        # index is count, val is num
+        freq = [[] for i in range(len(nums) + 1)]
         
         for n in nums:
-            d[n] = 1 + d.get(n, 0)
+            hashmap[n] = 1 + hashmap.get(n, 0)
             
-        heap = [(val, key) for key, val in d.items()]
-        
-        most_frequent = heapq.nlargest(k, heap)
+        for num, count in hashmap.items():
+            freq[count].append(num)
         
         res = []
         
-        for i in range(k):
-            res.append(most_frequent[i][1])
-            
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
+                
+                if len(res) == k:
+                    return res
+        
         return res
+        
+        
+#         d = {}
+        
+#         for n in nums:
+#             d[n] = 1 + d.get(n, 0)
+            
+#         heap = [(val, key) for key, val in d.items()]
+        
+#         most_frequent = heapq.nlargest(k, heap)
+        
+#         res = []
+        
+#         for i in range(k):
+#             res.append(most_frequent[i][1])
+            
+#         return res
