590720e281cc528055af152928c7739a1ad2980d
Krishna <herorocks56@gmail.com>
2023-08-11 21:51:04 -0700

    Time: 42 ms (84.89%), Space: 16.4 MB (66.49%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -3,19 +3,19 @@ class Solution:
         
         n = len(nums[0])
         s = "01"
-        res = []
-        
+
         def dfs(i, curStr):
             
             if len(curStr) == n:
                 if curStr not in nums:
-                    res.append(curStr)
-                return
+                    return curStr
+                return None
                     
             for i in range(2):
                 curStr = curStr+s[i]
-                dfs(i+1, curStr)
-                curStr = curStr[:-1]
+                res = dfs(i+1, curStr)
+                if res is not None:
+                    return res
+                curStr = curStr[:-1] 
             
-        dfs(0, "")
-        return res[0]
+        return dfs(0, "")
